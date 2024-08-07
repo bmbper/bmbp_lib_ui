ec6f9cd2dc4031c3d34b03054cf676f13854ed79
bmbp <zc_union@163.com>
2024-08-07 22:19:05 +0800

    V0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+mod router;
+pub use router::build_bmbp_ui_lib_router;
---
+++ src/router.rs
@@ -0,0 +1,12 @@
+use rust_embed::RustEmbed;
+use salvo::prelude::*;
+use salvo::serve_static::static_embed;
+
+#[derive(RustEmbed)]
+#[folder = "static"]
+struct BmbpStaticAssets;
+///
+/// bmbp/ui/lib/<**path>
+pub fn build_bmbp_ui_lib_router() -> Router {
+    Router::with_path("bmbp/ui/lib/<**path>").get(static_embed::<BmbpStaticAssets>())
+}
